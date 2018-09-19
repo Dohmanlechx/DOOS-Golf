feff3049b7b07a0beaa5f3094877f7602f4c5da9
David Öhman <dohman_92@hotmail.com>
2018-09-19 14:25:21 +0200

    Trying get ball physics right

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // Cached references
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+
+    // Variables
+    [SerializeField] public float releaseTime = 0.5f;
+    [SerializeField] public float maxDragDistance = 2f;
+
+    private bool isPressed = false;
+
+	void Update ()
+    {
+		if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+            else
+                rb.position = mousePos;
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+
+        yield return new WaitForSeconds(2f);
+    }
+}
