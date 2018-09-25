c4839aff3e20eda98bbe2f7d1ff62270d69eab0e
David Öhman <dohman_92@hotmail.com>
2018-09-25 14:27:40 +0200

    Attached offset to prevent the ball being under finger. Still in work though

--- Assets/Scripts/Club.cs
+++ Assets/Scripts/Club.cs
@@ -24,6 +24,8 @@ public class Club : MonoBehaviour
     private bool isPressed = false;
     private bool alreadyExecuted = false;
     private Vector3 ballPos;
+    private Vector2 inputPos;
+    private Vector3 inputOffset;
 
     // Start
     private void Start()
@@ -77,20 +79,22 @@ public class Club : MonoBehaviour
     // Updating the position, rotation etc for the club in realtime while preparing the shoot
     private void PreparingShoot()
     {
+        // Attching hook onto the ball 
         clubHook.gameObject.transform.position = theBall.transform.position;
 
-        Vector2 direction = ballTarget.position - transform.position;
+        // Facing the club to ball
+        Vector3 direction = ballTarget.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         rotation *= Quaternion.Euler(0, 0, -90);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (Vector3.Distance(mousePos, clubHookRb.position) > maxDragDistance)
-            clubRb.position = clubHookRb.position + (mousePos - clubHookRb.position).normalized * maxDragDistance;
+        // Positions of player's input
+        inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Vector3.Distance(inputPos, clubHookRb.position) > maxDragDistance) // TODO Need fix
+            clubRb.position = clubHookRb.position + (inputPos - clubHookRb.position).normalized * maxDragDistance;
         else
-            clubRb.position = mousePos;
+            clubRb.position = inputPos + (Vector2) inputOffset;
     }
 
     // Executes as soon as mouse click is down
@@ -98,6 +102,8 @@ public class Club : MonoBehaviour
     {
         if (clubRb.velocity.magnitude <= 0.02f) // Checks if ball is not moving
         {
+            inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            inputOffset = transform.position - (Vector3) inputPos;
             GetComponent<SpringJoint2D>().enabled = true;
             allowCameraMove = false;
             isPressed = true;
