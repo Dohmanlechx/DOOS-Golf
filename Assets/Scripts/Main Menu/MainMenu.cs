dfeb83b412d278b6b86ee34a61174b32c00edf52
SW4GL0RD <o.sarholm@hotmail.com>
2018-09-26 11:55:16 +0200

    Main menu added

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public bool isStandard;
+    public bool isChallenge;
+    public bool isQuit;
+
+    //TODO: Gör om siffrorna för scenerna till ett index så att man senare kan ladda in sin senaste bana.
+    private void OnMouseUp(){
+        if (isStandard){
+            SceneManager.LoadScene("Course 1");
+        }
+        if (isChallenge){
+            SceneManager.LoadScene("Challenge 1"); 
+        }
+        if (isQuit){
+            Application.Quit();
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
