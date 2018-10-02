76e3ace4de6d6bf79914df795a17d7f9fc1ea440
David Öhman <dohman_92@hotmail.com>
2018-10-02 15:02:44 +0200

    Collider sounds working

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,19 +7,28 @@ public class Ball : MonoBehaviour
     // Cached references
     public Rigidbody2D rb;
     public Club theClub;
+    public AudioSource audioSource;
     
     // Private variables
     [SerializeField] List<Transform> startPositions;
+    [SerializeField] List<AudioClip> sounds;
 
     // Public variables
     public int playersPositionChoice;
 
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         // 2 is the middle position, set as default
         playersPositionChoice = 2;
         rb = GetComponent<Rigidbody2D>();
         StartCoroutine(PositionTheBall());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        int randomSound = Random.Range(0, 3);
+        audioSource.PlayOneShot(sounds[randomSound], 1f);
     }
 
     // This coroutine is running as long player hasn't touched the club
