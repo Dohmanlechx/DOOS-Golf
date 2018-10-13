322214cf1296a599717e95b09dc7becd72da9377
SW4GL0RD <o.sarholm@hotmail.com>
2018-10-13 18:44:45 +0200

    Adding players etc

---
+++ Assets/Scripts/Player and Score/PlayerHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour {
+
+    public static ArrayList playerList = new ArrayList();
+
+    int[] player1Holes = new int[18];
+    int[] player2Holes = new int[18];
+    int[] player3Holes = new int[18];
+    int[] player4Holes = new int[18];
+
+    public int getPlayerAmount()
+    {
+        int p = playerList.Count;
+        return p;
+    }
+    
+}
---
+++ Assets/Scripts/Player and Score/ScoreBoard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreBoard : MonoBehaviour {
+
+    public List<TextMeshPro> player1Holes = new List<TextMeshPro>();
+    public List<TextMeshPro> player2Holes = new List<TextMeshPro>();
+    public List<TextMeshPro> player3Holes = new List<TextMeshPro>();
+    public List<TextMeshPro> player4Holes = new List<TextMeshPro>();
+    public PlayerHandler playerHandler;
+    private int amountPlayers;
+
+    public bool isContinue;
+
+    private void Start()
+    {
+        amountPlayers = playerHandler.getPlayerAmount();
+    }
+
+
+    private void OnMouseUp()
+    {
+        if (isContinue)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+
+}
