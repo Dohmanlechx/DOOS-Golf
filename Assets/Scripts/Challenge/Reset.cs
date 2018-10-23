54a70c5ee0af867d365efbbd6fc542442586a92a
David Öhman <dohman_92@hotmail.com>
2018-10-23 23:10:04 +0200

    Added back function in Challenge mode

--- Assets/Scripts/Challenge/Reset.cs
+++ Assets/Scripts/Challenge/Reset.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class Reset : MonoBehaviour
 {
+    private void Update()
+    {
+        // PC Escape & Android Back Button
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Main Menu");
+            return;
+        }
+    }
+
     void OnMouseDown()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
